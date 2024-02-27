80c6f8ed9cb8792f73b0f19601125eb54decd15f
Sachin <sachinph32@gmail.com>
2024-02-27 18:44:47 +0530

    initial commit

---
+++ student/src/test/java/com/sachin/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
